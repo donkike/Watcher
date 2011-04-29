c961a61dee86baeccb36439498f376fc52bbf5a7
aremun19 <dmunera119@gmail.com>
2011-04-29 11:49:59 -0500

    form style

--- apps/frontend/modules/genre/templates/_form.php
+++ apps/frontend/modules/genre/templates/_form.php
@@ -1,5 +1,6 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
+<?php use_stylesheet('form') ?>
 
 <form action="<?php echo url_for('genre/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
 <?php if (!$form->getObject()->isNew()): ?>
